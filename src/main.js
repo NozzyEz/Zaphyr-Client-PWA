9fce5373907a29aebe276180693b07faf1bdf549
Mark Sahlgreen <mark@nozzy.org>
2020-05-13 09:41:45 +0200

    added pwa functionality

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import { createProvider } from './vue-apollo'
 import Ionic from '@ionic/vue'
 import '@ionic/core/css/core.css'
 import '@ionic/core/css/ionic.bundle.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
